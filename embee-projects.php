eddae2965cc45b0191bf7da9d766fd84f47c1805
Marcus Battle <marcus@marcusbattle.com>
2014-07-03 20:21:32 -0400

    Add idea post type

--- embee-projects.php
+++ embee-projects.php
@@ -10,6 +10,50 @@
 * License: A "Slug" license name e.g. GPL2
 */
 
+
+/*
+*/
+function embee_idea_post_type() {
+
+	$labels = array(
+		'name'               => _x( 'Ideas', 'post type general name', 'your-plugin-textdomain' ),
+		'singular_name'      => _x( 'Idea', 'post type singular name', 'your-plugin-textdomain' ),
+		'menu_name'          => _x( 'Ideas', 'admin menu', 'your-plugin-textdomain' ),
+		'name_admin_bar'     => _x( 'Idea', 'add new on admin bar', 'your-plugin-textdomain' ),
+		'add_new'            => _x( 'Add New', 'idea', 'your-plugin-textdomain' ),
+		'add_new_item'       => __( 'Add New Idea', 'your-plugin-textdomain' ),
+		'new_item'           => __( 'New Idea', 'your-plugin-textdomain' ),
+		'edit_item'          => __( 'Edit Idea', 'your-plugin-textdomain' ),
+		'view_item'          => __( 'View Idea', 'your-plugin-textdomain' ),
+		'all_items'          => __( 'All Ideas', 'your-plugin-textdomain' ),
+		'search_items'       => __( 'Search Ideas', 'your-plugin-textdomain' ),
+		'parent_item_colon'  => __( 'Parent Ideas:', 'your-plugin-textdomain' ),
+		'not_found'          => __( 'No ideas found.', 'your-plugin-textdomain' ),
+		'not_found_in_trash' => __( 'No ideas found in Trash.', 'your-plugin-textdomain' )
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'ideas' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+	);
+
+	register_post_type( 'embee_idea', $args );		
+
+}
+
+add_action( 'init', 'embee_idea_post_type' );
+
+
 /*
 */
 function embee_project_post_type() {
@@ -481,18 +525,50 @@ function embee_task_ajax_update_task() {
 add_action( 'wp_ajax_nopriv_update_task', 'embee_task_ajax_update_task' );
 add_action( 'wp_ajax_update_task', 'embee_task_ajax_update_task' );
 
+
 /*
 */
-function embee_project_admin_bar() {
+function my_get_posts( $query ) {
 
-	if ( in_array( get_post_type(), array( 'embee_project', 'embee_task' ) ) ) {
-		return false;
+	if ( ( is_home() && $query->is_main_query() ) || is_feed() ) {
+		
+		$post_types = get_query_var('post_type');
+
+		if ( empty($post_types) )
+			$post_types[] = 'post';
+
+		$post_types[] = 'embee_idea';
+
+		$query->set( 'post_type', $post_types );
+
 	}
-	
+
+	return $query;
 }
 
-add_filter('show_admin_bar', 'embee_project_admin_bar');
+add_filter( 'pre_get_posts', 'my_get_posts' );
 
+function embee_idea_post_data( $data , $postarr ) {
+	
+	if ( $data['post_type'] == 'embee_idea' )
+		$data['post_status'] = 'private';
+
+	return $data;
+
+}
+
+add_filter( 'wp_insert_post_data', 'embee_idea_post_data', '99', 2 );
+
+function embee_idea_title_filter( $title ) {
+
+	if ( get_post_type() == 'embee_idea' ) 
+		$title = "Idea: " . $title;
+
+	return $title;
+
+}
+
+add_filter( 'the_title', 'embee_idea_title_filter' );
 
 function embee_include( $file_path ) {
 
